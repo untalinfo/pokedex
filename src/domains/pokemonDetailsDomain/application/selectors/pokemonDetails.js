7e7ef14e97a1e43492f347c234baba417a295288
Oscar Eduardo Info Giraldo <60395911+untalinfo@users.noreply.github.com>
2025-05-26 15:34:31 -0500

    fix route pokemon details page (#5)

--- src/domains/pokemonDetailsDomain/application/selectors/pokemonDetails.js
+++ src/domains/pokemonDetailsDomain/application/selectors/pokemonDetails.js
@@ -1,7 +1,15 @@
 import { createSelector } from '@reduxjs/toolkit';
 
-export const pokemonDetailState = (state) => state.pokemon;
+export const pokemonDetailState = (state) => state.details;
 
-export const getpokemonDetailslector = createSelector(pokemonDetailState, (pokemonDetail) => {
+export const getpokemonDetailSelector = createSelector(pokemonDetailState, (pokemonDetail) => {
 	return pokemonDetail?.details;
+});
+
+export const getpokemonDetailStatusSelector = createSelector(pokemonDetailState, (pokemonDetail) => {
+	return pokemonDetail?.status;
+});
+
+export const getpokemonDetailsErrorSelector = createSelector(pokemonDetailState, (pokemonDetail) => {
+	return pokemonDetail?.error;
 });
