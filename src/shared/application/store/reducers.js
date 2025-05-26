9a2d923e87cb7798b0e71692a4134a56f9adfe15
oscar Info <racso1607@gmail.com>
2025-05-26 14:02:08 -0500

    update readme and delete unecessary files

--- src/shared/application/store/reducers.js
+++ src/shared/application/store/reducers.js
@@ -1,15 +1,9 @@
 import { combineReducers } from 'redux';
-import example, { initialState as exampleInitial } from '../../../domains/exampleDomain/application/slices/example';
 import pokemon from '../../../domains/homeDomain/application/slices/pokemon';
 import types from '../../../domains/homeDomain/application/slices/types';
 import details from '../../../domains/pokemonDetailsDomain/application/slices/pokemonDetails';
 
-export const initialStates = {
-	example: exampleInitial,
-};
-
 export default combineReducers({
-	example,
 	pokemon,
 	types,
 	details,
